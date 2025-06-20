4d309b4fcf61396eabfd5b364ac3bae8a678237c
Razvan Andrei <chiper_raizy@yahoo.com>
2025-06-20 10:38:34 +0200

    [code] adding the routes to the backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from "express"; // import express for creating the server
 import { PORT } from "./config/config.js"; // import the PORT from config
-
+import authRoutes from "./routes/auth.route.js"; // import authentication routes
 const app = express(); // create an instance of express
 
 app.get("/", (req, res) => {
   res.send("app is running...");
 });
+
+app.use("/api/auth", authRoutes); // use auth routes for authentication
 
 app.listen(PORT, () => {
   // set the server to listen on port specified in config
