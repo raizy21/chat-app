9e8a5e5c99d14c3e559848a49cbf73f3cbd5c619
Razvan Andrei <chiper_raizy@yahoo.com>
2025-06-23 09:18:47 +0200

    [code] making the daisui work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 const App = () => {
   return (
-    <main
-      className="center
+    <div>
+      <main
+        className="center
    text-6xl font-bold underline"
-    >
-      chat app
-    </main>
+      >
+        chat app
+      </main>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
   );
 };
 
