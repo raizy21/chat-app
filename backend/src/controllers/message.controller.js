2e553ca6fb48899a6e68d89ddd6df8fd807be8cf
Razvan Andrei <chiper_raizy@yahoo.com>
2025-06-23 08:38:11 +0200

    [code] adding the messages with id

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -11,6 +12,25 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(200).json(filteredUsers);
   } catch (error) {
     console.error("error fetching users for sidebar:", error);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { sender: myId, receiver: userToChatId },
+        { sender: userToChatId, receiver: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("error fetching messages:", error);
     res.status(500).json({ error: "internal server error" });
   }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import {
+  getUsersForSidebar,
+  getMessages,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/user", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
