0f765d65a52f862e6c15188467aebec3b34fad55
Razvan Andrei <chiper_raizy@yahoo.com>
2025-06-29 10:12:52 +0200

    [code] making the authUser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,19 @@ import SettingsPage from "../src/pages/SettingsPage";
 import ProfilePage from "../src/pages/ProfilePage";
 import NotFoundPage from "../src/pages/NotFoundPage";
 
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 const App = () => {
+  const { authUser, checkAuth } = useAuthStore((state) => ({
+    authUser: state.authUser,
+  }));
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log("authUser in App:", authUser);
+
   return (
     <>
       <Routes>
